f2b599651e11b8a15f28d5b58325021b8a32868e
Ziana Aulia <zianaaulia2602@gmail.com>
2022-04-07 09:19:36 +0700

    Mengedit file Main Activity dengan Menambahkan fungsi logika.

--- app/src/main/java/zianaaulia/polbeng/ac/id/androidkotlinapp/MainActivity.kt
+++ app/src/main/java/zianaaulia/polbeng/ac/id/androidkotlinapp/MainActivity.kt
@@ -2,10 +2,22 @@ package zianaaulia.polbeng.ac.id.androidkotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvCounter.text = "1"
+    }
+    fun addNumber(v: View) {
+        val currVal = tvCounter.text.toString().toInt()
+        val nextVal = currVal + 1
+        tvCounter.text = nextVal.toString()
     }
 }
+
+/*Mengubah file Main Activity unguk membuat fungsi logika pada tampilan yang
+  dibuat di file activity_main.xml di folder layout*/
